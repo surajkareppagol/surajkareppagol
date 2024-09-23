397563fbeb491ac94dbaaac0032f4610878510ca
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-09-23 07:24:37 +0530

    [main] Update nvim script
    Changes:
      - Add messages
      - Handle errors

--- nvim/nvim.sh
+++ nvim/nvim.sh
@@ -2,29 +2,36 @@
 
 VERSION=0.10.1
 
-## Download nvim-linux64.tar.gz
-echo -e "\033[1;32mnvim\033[0m: Downloading tar..."
+## Check if nvim binary is present
 
-mkdir -p ~/.local/
-
-wget -q -nc -P ~/.local/ "https://github.com/neovim/neovim/releases/download/v$VERSION/nvim-linux64.tar.gz"
+NVIM_VERSION=$(nvim --version | cut -d"v" -f2 | head -n1)
 
 if [[ $? != 0 ]]
 then
-  echo "💥 Something went wrong."
-  exit
+  ## Download nvim-linux64.tar.gz
+  echo -e "\033[1;32mnvim\033[0m: Downloading tar..."
+
+  mkdir -p ~/.local/
+
+  wget -q -nc -P ~/.local/ "https://github.com/neovim/neovim/releases/download/v$VERSION/nvim-linux64.tar.gz"
+
+  if [[ $? != 0 ]]
+  then
+    echo "💥 Something went wrong."
+    exit
+  fi
+
+  ## Extract tar
+  echo -e "\033[1;32mnvim\033[0m: Extracting tar..."
+
+  tar -xzf ~/.local/nvim-linux64.tar.gz -C ~/.local/
+  rm ~/.local/nvim-linux64.tar.gz
+
+  ## Linking binary
+  echo -e "\033[1;32mnvim\033[0m: Linking binary..."
+
+  ln -sf ~/.local/nvim-linux64/bin/nvim ~/.local/bin/
 fi
-
-## Extract tar
-echo -e "\033[1;32mnvim\033[0m: Extracting tar..."
-
-tar -xzf ~/.local/nvim-linux64.tar.gz -C ~/.local/
-rm ~/.local/nvim-linux64.tar.gz
-
-## Linking binary
-echo -e "\033[1;32mnvim\033[0m: Linking binary..."
-
-ln -sf ~/.local/nvim-linux64/bin/nvim ~/.local/bin/
 
 ## Write the ~/.local/bin to PATH
 CURRENT_SHELL=$(echo $SHELL | rev | cut -d"/" -f1 | rev)
